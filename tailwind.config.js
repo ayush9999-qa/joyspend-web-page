78d0466522102982a452e3b0a6fb9a255f774b40
Ayush Goel <ayush9999@gmail.com>
2026-03-25 23:21:22 +0700

    Added seo optimizations

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./blog/*.html",
+    "./blog/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
